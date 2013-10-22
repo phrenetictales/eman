bf681d15b47dbbb8996d4e8ad00776958868fe67
Phillip Whelan <pwhelan@mixxx.org>
2013-10-21 20:20:56 -0700

    FIX: expose view variables to the main template, header and footer.

--- lib/Phrenetic/SlimMustacheView.php
+++ lib/Phrenetic/SlimMustacheView.php
@@ -2,6 +2,9 @@
 
 namespace Phrenetic;
 
+use Cartalyst\Sentry\Facades\Native\Sentry as Sentry;
+
+
 class SlimMustacheView extends \Slim\View
 {
 	protected $_engine = NULL;
@@ -20,6 +23,20 @@ class SlimMustacheView extends \Slim\View
 		$page = $this->_engine->loadTemplate('content');
 		$m = $this->_engine->loadTemplate($tpl);
 		
-		return $page->render(['main' => $m->render($this->data)]);
+		if (Sentry::check()) {
+			$user = Sentry::getUser();
+			$this->appendData(['user' => [
+				'login' => $user->getLogin(),
+				'id'	=> $user->getID()
+			]]);
+		}
+		else {
+			$this->appendData(['user' => null]);
+		}
+		
+		
+		$this->appendData(['main' => $m->render($this->data)]);
+		
+		return $page->render($this->data);
 	}
 }
